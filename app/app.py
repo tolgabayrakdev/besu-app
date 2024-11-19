8fd0a5415800d4a3a31534d5875dd8278f946e02
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-11-19 09:24:14 +0300

    besu

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from .model import db
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://root:root@localhost:5432/postgres"
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ app/model.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import check_password_hash
+from sqlalchemy.orm import Mapped, mapped_column
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "created_at": self.created_at,
+        }
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
